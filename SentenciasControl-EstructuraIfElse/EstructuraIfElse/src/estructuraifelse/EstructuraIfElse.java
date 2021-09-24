3a879305cd61be199879b4c586a55b8636893ef8
MateoReinoso <mateo_riva@hotmail.com>
2021-09-23 20:55:39 -0500

    Esructura control

---
+++ SentenciasControl-EstructuraIfElse/EstructuraIfElse/src/estructuraifelse/EstructuraIfElse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuraifelse;
+
+/**
+ *
+ * @author mateo
+ */
+public class EstructuraIfElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        var condicion = true;
+        if (condicion) {
+            System.out.println("Condicion verdadera");
+        } else {
+            System.out.println("Condicion falsa");
+        }
+    }
+
+}
